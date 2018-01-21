fa4bd7334fc9a1fd751ef8749a35b7da95d578a9
martinproxy0 <marcinsornek@gmail.com>
2018-01-21 19:03:52 +0100

    Add basic code

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './css/App.css';
+import Title from '../components/Title';
+import TodoList from '../components/TodoList';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {
+                    id: 1,
+                    text: 'Some todo text 1'
+                },
+                {
+                    id: 2,
+                    text: 'Some todo text 2'
+                },
+                {
+                    id: 3,
+                    text: 'Some todo text 3'
+                }
+            ]
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({data: remainder});
+    }
+    render() {
+        return (
+            <div className={style.TodoApp}>
+                <Title title='abc' noumber={this.state.data.length} />  
+                <TodoList items={this.state.data} remove={(id) => this.removeTodo(id)} />               
+            </div> 
+        );
+    }
+}
+
+export default App;
